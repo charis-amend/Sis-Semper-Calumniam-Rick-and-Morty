942c622659e0acecffa102361b84d2380ff39dc9
Charis Amend <charis.amend@googlemail.com>
2024-01-10 12:59:26 +0100

    added variables for characters instead of dataApi.results, and maxPage variable and added page in function andd in url

--- index.js
+++ index.js
@@ -11,20 +11,28 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
-const page = 1;
+// const maxPage = 42;
+let page = 1;
 const searchQuery = "";
 
 // fetchCharacters function -- get the first 20 characters from the API
-async function fetchCharacters() {
+async function fetchCharacters(page) {
   try {
-    const response = await fetch("https://rickandmortyapi.com/api/character/1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20")
+    const response = await fetch(`https://rickandmortyapi.com/api/character/?page=${page}`)
 
     if (response.ok) {
-      const characters = await response.json();
-      console.log(characters)
+
+      // getting everything from the api
+      const dataApi = await response.json();
+      console.log(dataApi)
+
+      // getting maxPage = 42 from api
+      const maxPage = dataApi.info.pages;
+
       // emptying out cardContainer: 
       cardContainer.innerHTML = '';
+
+      const characters = dataApi.results;
 
       characters.forEach((character) => { createCharacterCard(character) });
     }
@@ -33,4 +41,4 @@ async function fetchCharacters() {
     console.error("the API rickmorty is not fetching, see index.js fetchCharacters()", error)
   }
 }
-fetchCharacters();
+fetchCharacters(page);
