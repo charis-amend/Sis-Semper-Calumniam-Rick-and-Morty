f18d4580097930468741d8220d23246e78acaed4
Charis Amend <charis.amend@googlemail.com>
2024-01-10 10:35:50 +0100

    added fetch url and 20 characters api link

--- index.js
+++ index.js
@@ -12,3 +12,9 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+// fetchCharacters function -- get the first 20 characters from the API
+function fetchCharacters() {
+  const response = fetch("https://rickandmortyapi.com/api/character/1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20")
+  console.log(response)
+}
